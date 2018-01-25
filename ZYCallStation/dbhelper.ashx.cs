9158bcd829f7586179f5c505dcc817beb7fc2dcf
togetupinfo <370842652@qq.com>
2018-01-25 18:24:32 +0800

    init
    签入

---
+++ ZYCallStation/dbhelper.ashx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZYCallStation
+{
+    /// <summary>
+    /// dbhelper 的摘要说明
+    /// </summary>
+    public class dbhelper : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            switch (context.Request.Params["keyword"].ToString())
+            {
+                case "getconfig":
+
+                    break;
+                default:
+                    break;
+            }
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public string getconfig(HttpRequest httpreq) {
+            string clientip = httpreq.Params["ip"];
+
+
+            return "";
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
